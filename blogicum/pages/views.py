b52a5157363c31625880792d2435aeb26b6a814a
Ваня Бабаев <ivan.babaev.sun@gmail.com>
2023-12-14 00:53:16 +0300

    2-st version

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -5,6 +5,7 @@ def about(request):
     template = 'pages/about.html'
     return render(request, template)
 
+
 def rules(request):
     template = 'pages/rules.html'
     return render(request, template)
